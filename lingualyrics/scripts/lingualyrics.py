c8e37ea7776841674a0554ffe0bd63cf43a01f0e
Habib Kazemi <kazemihabib1996@gmail.com>
2018-03-14 15:03:46 +0330

    Remove playerctl dependency

--- lingualyrics/scripts/lingualyrics.py
+++ lingualyrics/scripts/lingualyrics.py
@@ -1,35 +1,61 @@
 #!/usr/bin/env python3
-from gi.repository import Playerctl, GLib
+from gi.repository import GLib
+import dbus
 import lyric
+from dbus.mainloop.glib import DBusGMainLoop
+DBusGMainLoop(set_as_default=True)
 
-player = Playerctl.Player(player_name='clementine')
+last_track = ('', '')
 
-last_track = ('','')
+player = dbus.SessionBus().get_object('org.mpris.MediaPlayer2.clementine',
+                                      '/org/mpris/MediaPlayer2')
 
-def on_metadata(player, e):
+
+def on_metadata(*args, **kwargs):
+
     global last_track
-    if 'xesam:artist' in e.keys() and 'xesam:title' in e.keys():
-        artist = e['xesam:artist'][0]
-        title = e['xesam:title']
-        #Got this signal multiple time
-        if (artist, title) == last_track:
-            return
 
+    metadata = player.Get('org.mpris.MediaPlayer2.Player', 'Metadata')
+    artist = ''
+    title = ''
+
+    if 'xesam:artist' in metadata:
+        artist = metadata['xesam:artist'][0]
+    else:
+        print("Sorry, Track info has no artist name")
+        return
+    if 'xesam:title' in metadata:
+        title = metadata['xesam:title']
+    else:
+        print("Sorry, Track info has no title")
+        return
+
+    if (artist, title) == last_track:
+        return
+
+    # Got this signal multiple time
+    last_track = (artist, title)
+    get_lyric(artist, title)
+
+
+def get_lyric(artist, title):
         print('Now playing', end=": ")
-        print('{_artist} - {_title}'.format(_artist= artist, _title=title))
+        print('{_artist} - {_title}'.format(_artist=artist, _title=title))
         print('\n')
         print("*" * 20)
         lyric_text = lyric.get_lyric(artist, title)
         if lyric_text is not None:
             print(lyric_text)
         else:
-            print("Sorry! no lyric found") 
+            print("Sorry! no lyric found")
 
-        last_track = (artist,title)
         print("*" * 20)
         print('\n')
 
-player.on('metadata', on_metadata)
+
+player.connect_to_signal("PropertiesChanged", on_metadata)
+metadata = player.Get('org.mpris.MediaPlayer2.Player', 'Metadata')
+on_metadata()
 
 # wait for events
 main = GLib.MainLoop()
--- lingualyrics/scripts/lyric.py
+++ lingualyrics/scripts/lyric.py
@@ -1,7 +1,14 @@
 import requests
 from xml.etree import ElementTree
 
-def get_lyric(artis, title):
-    r = requests.get('http://api.chartlyrics.com/apiv1.asmx/SearchLyricDirect?artist={0}&song={1}'.format(artis,title))
-    tree = ElementTree.fromstring(r.content)
-    return tree.find('{http://api.chartlyrics.com/}Lyric').text
+
+def get_lyric(artist, title):
+    r = requests.get('http://api.chartlyrics.com/apiv1.asmx/SearchLyricDirect\
+                     ?artist={0}&song={1}'.format(artist, title))
+    # print(r.content)
+    try:
+        tree = ElementTree.fromstring(r.content)
+        return tree.find('{http://api.chartlyrics.com/}Lyric').text
+    except Exception:
+        return None
+        # print("XML parse Error", Exception)
